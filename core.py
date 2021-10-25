db63b4b76f3006fb2ce2602c060621d4e40c80fc
JimOhman <jim.ohm@outlook.com>
2021-10-25 20:24:59 +0200

    increased speed by another ~20% by using a JIT compiler for the memory module

--- core.py
+++ core.py
@@ -2,9 +2,10 @@ import torch.nn.functional as F
 import torch.nn as nn
 import torch
 import numpy as np
+from torch import jit
 
 
-class TemporalLinkage(nn.Module):
+class TemporalLinkage(jit.ScriptModule):
 
   def __init__(self, batch_size, capacity, num_writes):
     super(TemporalLinkage, self).__init__()
@@ -12,25 +13,29 @@ class TemporalLinkage(nn.Module):
     self.num_writes = num_writes
     self.capacity = capacity
 
-    link = torch.zeros((batch_size, num_writes, capacity, capacity))
-    precedence_weights = torch.zeros((batch_size, num_writes, capacity))
-    zero_diagonal = 1 - torch.eye(capacity, capacity).view(1, 1, -1, capacity)
-
-    self.register_buffer('link', link, persistent=False)
-    self.register_buffer('precedence_weights', precedence_weights, persistent=False)
-    self.register_buffer('zero_diagonal', zero_diagonal, persistent=False)
+    self.link = torch.zeros((batch_size, num_writes, capacity, capacity))
+    self.precedence_weights = torch.zeros((batch_size, num_writes, capacity))
+    self.zero_diagonal = 1 - torch.eye(capacity, capacity).view(1, 1, -1, capacity)
 
     self.strengths_op = nn.Softplus()
     self.epsilon = 1e-6
+
+  def to(self, device):
+    super(TemporalLinkage, self).to(device)
+    self.link = self.link.to(device)
+    self.precedence_weights = self.precedence_weights.to(device)
+    self.zero_diagonal = self.zero_diagonal.to(device)
 
   def reset(self, device):
     self.link = torch.zeros((self.batch_size, self.num_writes, self.capacity, self.capacity), device=device)
     self.precedence_weights = torch.zeros((self.batch_size, self.num_writes, self.capacity), device=device)
 
+  @jit.script_method
   def update_precedence_weights(self, write_weights):
-    write_sum = torch.sum(write_weights, 2, keepdims=True)
+    write_sum = torch.sum(write_weights, 2, keepdim=True)
     self.precedence_weights = (1 - write_sum) * self.precedence_weights + write_weights
 
+  @jit.script_method
   def update_link(self, write_weights):
     write_weights_i = write_weights.unsqueeze(3)
     write_weights_j = write_weights.unsqueeze(2)
@@ -39,12 +44,14 @@ class TemporalLinkage(nn.Module):
     prev_link_scale = 1 - write_weights_i - write_weights_j
     self.link = (prev_link_scale * self.link + new_link) * self.zero_diagonal
 
+  @jit.script_method
   def sharpening(self, address, strengths):
     transformed_strengths = self.strengths_op(strengths)
     address = (address + self.epsilon)**(transformed_strengths)
     return address / address.sum(dim=-1, keepdim=True)
 
-  def directional_read_weights(self, prev_read_weights, forward, strengths):
+  @jit.script_method
+  def directional_read_weights(self, prev_read_weights, forward: bool, strengths):
     expanded_read_weights = prev_read_weights.unsqueeze(1).expand(-1, self.num_writes, -1, -1)
     if forward:
       result = torch.matmul(expanded_read_weights, self.link.transpose(-1, -2))
@@ -54,20 +61,20 @@ class TemporalLinkage(nn.Module):
       result = self.sharpening(result, strengths)
     return result.transpose(1, 2)
 
+  @jit.script_method
   def update(self, write_weights):
     self.update_link(write_weights)
     self.update_precedence_weights(write_weights)
 
 
-class Freeness(nn.Module):
+class Freeness(jit.ScriptModule):
 
   def __init__(self, batch_size, capacity):
     super(Freeness, self).__init__()
     self.batch_size = batch_size
     self.capacity = capacity
 
-    usage = torch.zeros(batch_size, capacity)
-    self.register_buffer('usage', usage, persistent=False)
+    self.usage = torch.zeros(batch_size, capacity)
 
     self.softmax = nn.Softmax(dim=-1)
     self.strengths_op = nn.Softplus()
@@ -75,16 +82,23 @@ class Freeness(nn.Module):
   def reset(self, device):
     self.usage = torch.zeros(self.batch_size, self.capacity, device=device)
 
+  def to(self, device):
+    super(Freeness, self).to(device)
+    self.usage = self.usage.to(device)
+
+  @jit.script_method
   def _allocation(self, usage, strengths):
     transformed_strengths = self.strengths_op(strengths)
     sharp_nonusage = (1 - usage) * transformed_strengths
     return self.softmax(sharp_nonusage)
   
+  @jit.script_method
   def _usage_after_write(self, write_weights):
     write_weights = 1 - torch.prod(1 - write_weights, 1)
     usage = self.usage + (1 - self.usage) * write_weights
     return usage
 
+  @jit.script_method
   def _usage_after_read(self, free_gate, read_weights):
     free_gate = free_gate.unsqueeze(-1)
     free_read_weights = free_gate * read_weights
@@ -92,7 +106,8 @@ class Freeness(nn.Module):
     usage = self.usage * phi
     return usage, phi
   
-  def write_allocation_weights(self, write_gates, num_writes, strengths):
+  @jit.script_method
+  def write_allocation_weights(self, write_gates, num_writes: int, strengths):
     write_gates = write_gates.unsqueeze(-1)
     temp_usage = self.usage
     allocation_weights = []
@@ -101,13 +116,14 @@ class Freeness(nn.Module):
       temp_usage = temp_usage + ((1 - temp_usage) * write_gates[:, i, :] * allocation_weights[i])
     return torch.stack(allocation_weights, dim=1)
 
+  @jit.script_method
   def update(self, write_weights, free_gate, read_weights):
     self.usage = self._usage_after_write(write_weights)
     self.usage, phi = self._usage_after_read(free_gate, read_weights)
     return phi
 
 
-class CosineWeights(nn.Module):
+class CosineWeights(jit.ScriptModule):
 
   def __init__(self):
     super(CosineWeights, self).__init__()
@@ -115,11 +131,13 @@ class CosineWeights(nn.Module):
     self.strengths_op = nn.Softplus()
     self.epsilon = 1e-6
   
+  @jit.script_method
   def weighted_softmax(self, activations, strengths):
     transformed_strengths = self.strengths_op(strengths)
     sharp_activations = activations * transformed_strengths
     return self.softmax(sharp_activations)
 
+  @jit.script_method
   def forward(self, memory, keys, strengths, mask):
     masked_keys = mask * keys
     masked_memory = torch.einsum('ihk,ijk->ihjk', mask, memory)
@@ -131,7 +149,7 @@ class CosineWeights(nn.Module):
     return self.weighted_softmax(similarity, strengths)
 
 
-class Memory(nn.Module):
+class Memory(jit.ScriptModule):
 
   def __init__(self, batch_size, input_dim, capacity, memory_dim, num_reads=1,
                                                                   num_writes=1,
@@ -145,12 +163,9 @@ class Memory(nn.Module):
     self.num_read_modes = 1 + 2 * self.num_writes
     self.strengths_dim = capacity if free_strengths else 1
 
-    memories = torch.ones((batch_size, capacity, memory_dim))
-    write_weights = torch.zeros(batch_size, num_writes, capacity)
-    read_weights = torch.zeros(batch_size, num_reads, capacity)
-    self.register_buffer('memories', memories, persistent=False)
-    self.register_buffer('write_weights', write_weights, persistent=False)
-    self.register_buffer('read_weights', read_weights, persistent=False)
+    self.memories = torch.ones((batch_size, capacity, memory_dim))
+    self.write_weights = torch.zeros(batch_size, num_writes, capacity)
+    self.read_weights = torch.zeros(batch_size, num_reads, capacity)
 
     self.linkage = TemporalLinkage(batch_size, capacity, num_writes)
     self.freeness = Freeness(batch_size, capacity)
@@ -181,9 +196,17 @@ class Memory(nn.Module):
 
     self.mode_strengths = nn.Linear(input_dim, num_writes * num_reads * self.strengths_dim)
 
-    self.gates = {'write_gate': None,
-                  'free_gate': None,
-                  'allocation_gate': None}
+    self.gates = {'write_gate': torch.zeros(0),
+                  'free_gate': torch.zeros(0),
+                  'allocation_gate': torch.zeros(0)}
+
+  def to(self, device):
+    super(Memory, self).to(device)
+    self.linkage.to(device)
+    self.freeness.to(device)
+    self.memories = self.memories.to(device)
+    self.write_weights = self.write_weights.to(device)
+    self.read_weights = self.read_weights.to(device)
 
   def reset(self, device):
     self.linkage.reset(device)
@@ -192,6 +215,7 @@ class Memory(nn.Module):
     self.write_weights = torch.zeros(self.batch_size, self.num_writes, self.capacity, device=device)
     self.read_weights = torch.zeros(self.batch_size, self.num_reads, self.capacity, device=device)
 
+  @jit.script_method
   def _read_inputs(self, inputs):
     write_vectors = self.write_vectors(inputs)
     write_vectors = write_vectors.view(-1, self.batch_size, self.num_writes, self.memory_dim)
@@ -236,55 +260,71 @@ class Memory(nn.Module):
                   'allocation_gate': allocation_gate}
 
     time_steps = inputs.shape[0]
-    for t in range(time_steps):
-      yield {'read_content_keys': read_keys[t],
-             'read_content_strengths': read_strengths[t],
-             'write_content_keys': write_keys[t],
-             'write_content_strengths': write_strengths[t],
-             'write_vectors': write_vectors[t],
-             'erase_vectors': erase_vectors[t],
-             'free_gate': free_gate[t],
-             'allocation_gate': allocation_gate[t],
-             'write_gate': write_gate[t],
-             'read_mode': read_mode[t],
-             'allocation_strengths': allocation_strengths[t],
-             'write_mask': write_mask[t],
-             'read_mask': read_mask[t],
-             'mode_strengths': mode_strengths[t]}
+    result = {'read_content_keys': read_keys,
+              'read_content_strengths': read_strengths,
+              'write_content_keys': write_keys,
+              'write_content_strengths': write_strengths,
+              'write_vectors': write_vectors,
+              'erase_vectors': erase_vectors,
+              'free_gate': free_gate,
+              'allocation_gate': allocation_gate,
+              'write_gate': write_gate,
+              'read_mode': read_mode,
+              'allocation_strengths': allocation_strengths,
+              'write_mask': write_mask,
+              'read_mask': read_mask,
+              'mode_strengths': mode_strengths}
+    return result, time_steps
 
-  def update_write_weights(self, inputs):
+  @jit.script_method
+  def update_write_weights(self, write_content_keys, 
+                                 write_content_strengths, 
+                                 write_mask,
+                                 allocation_gate,
+                                 allocation_strengths,
+                                 write_gate):
+
     write_content_weights = self._write_content_weights_mod(self.memories, 
-                                                            inputs['write_content_keys'],
-                                                            inputs['write_content_strengths'],
-                                                            inputs['write_mask'])
+                                                            write_content_keys,
+                                                            write_content_strengths,
+                                                            write_mask)
 
-    combined_gate = inputs['allocation_gate'] * inputs['write_gate']
+    combined_gate = allocation_gate * write_gate
     write_allocation_weights = self.freeness.write_allocation_weights(combined_gate,
                                                                       self.num_writes,
-                                                                      inputs['allocation_strengths'])
+                                                                      allocation_strengths)
 
-    allocation_gate = inputs['allocation_gate'].unsqueeze(-1)
-    write_gate = inputs['write_gate'].unsqueeze(-1)
+    allocation_gate = allocation_gate.unsqueeze(-1)
+    write_gate = write_gate.unsqueeze(-1)
 
     allocation = (allocation_gate * write_allocation_weights)
     content = (1 - allocation_gate) * write_content_weights
 
     self.write_weights = write_gate * (allocation + content)
 
-  def update_read_weights(self, inputs):
+  @jit.script_method
+  def update_read_weights(self, read_content_keys,
+                                read_content_strengths,
+                                read_mask,
+                                read_mode,
+                                mode_strengths):
+
     content_weights = self._read_content_weights_mod(self.memories,
-                                                     inputs['read_content_keys'],
-                                                     inputs['read_content_strengths'],
-                                                     inputs['read_mask'])
+                                                     read_content_keys,
+                                                     read_content_strengths,
+                                                     read_mask)
 
-    backward_weights = self.linkage.directional_read_weights(self.read_weights, forward=False,
-                                                             strengths=inputs['mode_strengths'])
-    forward_weights = self.linkage.directional_read_weights(self.read_weights, forward=True,
-                                                            strengths=inputs['mode_strengths'])
+    backward_weights = self.linkage.directional_read_weights(self.read_weights, 
+                                                             forward=False,
+                                                             strengths=mode_strengths)
 
-    backward_mode = inputs['read_mode'][..., :self.num_writes]
-    forward_mode = inputs['read_mode'][..., self.num_writes:2*self.num_writes]
-    content_mode = inputs['read_mode'][..., 2*self.num_writes]
+    forward_weights = self.linkage.directional_read_weights(self.read_weights,
+                                                            forward=True,
+                                                            strengths=mode_strengths)
+
+    backward_mode = read_mode[..., :self.num_writes]
+    forward_mode = read_mode[..., self.num_writes:2*self.num_writes]
+    content_mode = read_mode[..., 2*self.num_writes]
 
     backward = torch.sum(backward_mode.unsqueeze(3) * backward_weights, 2)
     forward = torch.sum(forward_mode.unsqueeze(3) * forward_weights, 2)
@@ -292,6 +332,7 @@ class Memory(nn.Module):
 
     self.read_weights = content + forward + backward
 
+  @jit.script_method
   def _erase_and_write(self, erase_vectors, write_vectors, phi):
     write_weights = self.write_weights.unsqueeze(3)
     erase_vectors = erase_vectors.unsqueeze(2)
@@ -303,17 +344,31 @@ class Memory(nn.Module):
     add_matrix = torch.matmul(self.write_weights.transpose(-1, -2), write_vectors)
     self.memories = full_reset * self.memories + add_matrix
 
+  @jit.script_method
   def forward(self, inputs):
-    inputs = self._read_inputs(inputs)
+    inputs, time_steps = self._read_inputs(inputs)
     memory_outputs = []
-    for input_ in inputs:
-      phi = self.freeness.update(self.write_weights, input_['free_gate'], self.read_weights)
-      self.update_write_weights(input_)
+    for t in range(time_steps):
+      phi = self.freeness.update(self.write_weights, inputs['free_gate'][t], self.read_weights)
 
-      self._erase_and_write(input_['erase_vectors'], input_['write_vectors'], phi)
+      self.update_write_weights(inputs['write_content_keys'][t],
+                                inputs['write_content_strengths'][t],
+                                inputs['write_mask'][t],
+                                inputs['allocation_gate'][t],
+                                inputs['allocation_strengths'][t],
+                                inputs['write_gate'][t])
+
+      self._erase_and_write(inputs['erase_vectors'][t], 
+                            inputs['write_vectors'][t],
+                            phi)
 
       self.linkage.update(self.write_weights)
-      self.update_read_weights(input_)
+
+      self.update_read_weights(inputs['read_content_keys'][t],
+                               inputs['read_content_strengths'][t],
+                               inputs['read_mask'][t],
+                               inputs['read_mode'][t],
+                               inputs['mode_strengths'][t])
 
       memory_output = torch.matmul(self.read_weights, self.memories)
       memory_outputs.append(memory_output)
@@ -337,6 +392,10 @@ class Controller(nn.Module):
     self.fc_input_dim = args.num_reads * args.memory_dim
     self.fc_output_dim = args.output_dim
     self.fc = nn.Linear(self.fc_input_dim, self.fc_output_dim)
+
+  def to(self, device):
+    super(Controller, self).to(device)
+    self.memory.to(device)
   
   def forward(self, x):
     memory_outputs = self.memory(x).view(-1, self.fc_input_dim)
